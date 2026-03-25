518548be060cddc63e50de6b4ce696635ee09335
VinayRajput2802 <vinayvrrajput123@gmail.com>
2026-03-25 16:36:12 +0530

    Create entities User and Project

--- src/main/java/com/capgemini/taskmanagementsystem/entity/Project.java
+++ src/main/java/com/capgemini/taskmanagementsystem/entity/Project.java
@@ -1,4 +1,34 @@
 package com.capgemini.taskmanagementsystem.entity;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="project")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Project {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="ProjectID")
+    private Integer projectId;
+    @Column(name="ProjectName",nullable = false)
+    private String projectName;
+    @Column(name="Description")
+    private String description;
+    @Column(name="StartDate")
+    private LocalDate startDate;
+    @Column(name = "EndDate")
+    private LocalDate endDate;
+
+    @ManyToOne()
+    @JoinColumn(name="UserId",nullable = false)
+    private User user;
 }
--- src/main/java/com/capgemini/taskmanagementsystem/entity/User.java
+++ src/main/java/com/capgemini/taskmanagementsystem/entity/User.java
@@ -1,4 +1,39 @@
 package com.capgemini.taskmanagementsystem.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name="user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="UserId")
+    private Integer userId;
+    @NonNull
+    @Column(name="Username")
+    private String username;
+    @NonNull
+    @Column(name="Password")
+    private String password;
+    @NonNull
+    @Column(name="Email")
+    private String email;
+    @NonNull
+    @Column(name="FullName")
+    private String fullName;
+
+    public User(){};
+
+    public User(Integer userId, @NonNull String username, @NonNull String password, @NonNull String email, @NonNull String fullName) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.fullName = fullName;
+    }
 }
