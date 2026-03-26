9fc635f95a422dfa17048baec8d49555649ac2a8
adityaKumar0909 <kadi93030@gmail.com>
2026-03-26 22:51:48 +0530

    added exception handling for notification and userrole entities

---
+++ src/main/java/com/capgemini/taskmanagementsystem/controller/UserRoleController.java
@@ -0,0 +1,9 @@
+package com.capgemini.taskmanagementsystem.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/userrole")
+public class UserRoleController {
+}
