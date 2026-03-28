20e501a1a6a121aba971a5baa75c31e7feb0aa9d
adityaKumar0909 <kadi93030@gmail.com>
2026-03-28 09:23:25 +0530

    resolved merge conflict

--- src/main/java/com/capgemini/taskmanagementsystem/exception/notification/InvalidNotificationRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.capgemini.taskmanagementsystem.exception.notification;
-
-public class InvalidNotificationRequestException extends RuntimeException {
-    public InvalidNotificationRequestException(String message) {
-        super(message);
-    }
-}
